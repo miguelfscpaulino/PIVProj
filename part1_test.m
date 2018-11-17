47c1cb84d6351927dadfce54a56433a68bd81086
miguelfscpaulino <miguel95paulino@gmail.com>
2018-11-17 18:50:36 +0000

    put imgs into folder

--- part1_test.m
+++ part1_test.m
@@ -1,15 +1,18 @@
+imgs_folder = 'images';
 
-img_rgb=dir('*.jpg');
-img_depth=dir('*.mat');
+img_rgb=dir(fullfile(imgs_folder,'*.jpg'));
+img_depth=dir(fullfile(imgs_folder,'*.mat'));
 
-imgseq1=repmat(struct('rgb',img_rgb(1).name,'depth',img_depth(2).name), length(img_rgb), 1);
+
+
+imgseq1=repmat(struct('rgb',fullfile(imgs_folder,img_rgb(1).name),'depth',fullfile(imgs_folder,img_depth(2).name)), length(img_rgb), 1);
 
 for i=2:length(img_rgb)
 
-    imgseq1(i)=struct('rgb',img_rgb(i).name,'depth',img_depth(i).name);
+    imgseq1(i)=struct('rgb',fullfile(imgs_folder,img_rgb(i).name),'depth',fullfile(imgs_folder,img_depth(i).name));
 
 end
 
 load('cameraparametersAsus.mat');
 
-[obj]=track3D_part1(imgseq1, cam_params);
+[obj]=track3D_part12(imgseq1, cam_params);
